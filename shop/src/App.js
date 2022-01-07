fde0df01663dd02059c3554e0dd1ceded1dedfd6
sypar <sypark@xprime.co.kr>
2022-01-07 18:39:33 +0900

    ff

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,26 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import { Navbar,Nav, NavDropdown, Form, Button, FormControl, Container } from 'react-bootstrap';
+import { sampleList } from './data.js';
+import Detail from './Detail.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <button type="button" class="btn btn-primary">Primary</button>
-        </a>
-      </header>
-    </div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
+          <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <Route exact path="/">
+        <div className="jumbotron">
+          <div className='jumbotron-container'>
+            <div className="background">
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            { 
+              sampleList.map(function(data, idx) {
+                return <Card data={ data } key={ data.id }/>
+              })
+            }
+          </div>
+        </div>
+      </Route>
+      <Switch>
+        <Route path="/detail">
+          <Detail/>
+        </Route>
+        <Route path="/:id">
+            <div>새로 만든 route입니다.</div>
+        </Route>
+      </Switch>
+  </div>
   );
+}
+
+function Card(obj) {
+  let data = obj.data;
+  return (
+    <div className="col-md-4">
+      <img src={ data.src } width="100%" />
+      <h4>{ data.title }</h4>
+      <p>{ data.content }
+        <br/> { data.price }
+      </p>
+    </div>
+  )
 }
 
 export default App;
---
+++ shop/src/Detail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom'
+
+function Detail() {
+    let history = useHistory();
+    
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" />
+                </div>
+                <div className="col-md-6 mt-4">
+                    <h4 className='pt-5'>상품명</h4>
+                    <p>상품 설명</p>
+                    <p></p>
+                    <button className="btn btn-danger">주문하기</button>
+                    <button className="btn btn-defaul" onClick={()=>{ history.goBack() }}>뒤로가기</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Detail
